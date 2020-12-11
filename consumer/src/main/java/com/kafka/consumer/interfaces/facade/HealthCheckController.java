db386f80936e225d725650fe8c2ef284b0d718cf
15014006969 <192900254@qq.com>
2020-12-11 17:06:20 +0800

    modified:启动类去掉冗余代码

--- consumer/src/main/java/com/kafka/consumer/interfaces/facade/HealthCheckController.java
+++ consumer/src/main/java/com/kafka/consumer/interfaces/facade/HealthCheckController.java
@@ -26,25 +26,6 @@ public class HealthCheckController {
     @Value("${spring.profiles.active}")
     private String profilesActive;
 
-
-    /**
-     * @api {get} /ping ping一下应用服务
-     * @apiDescription 测试应用是否正常运行
-     * @apiVersion 1.0.0
-     * @apiGroup Test
-     * @apiSuccess {String} message  消息
-     * @apiSuccess {String} profilesActive  环境
-     * @apiSuccess {String} applicationName  应用名
-     * @apiSuccess {String} serverPort  端口
-     * @apiSuccessExample Success-Response:
-     * {
-     * "message":"pong!",
-     * "profilesActive":"dev",
-     * "applicationName": "membercenter-api",
-     * "serverPort": 8073
-     * }
-     * @apiSampleRequest /ping
-     */
     @GetMapping(value = "ping")
     @ResponseBody
     public Map<String, Object> healthCheck() {
--- producer/src/main/java/com/kafka/producer/ProducerApplication.java
+++ producer/src/main/java/com/kafka/producer/ProducerApplication.java
@@ -3,7 +3,7 @@ package com.kafka.producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.kafka.producer", "com.kafka.common"})
+@SpringBootApplication
 public class ProducerApplication {
 
     public static void main(String[] args) {
